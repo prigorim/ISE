c4a234677ba6362aa6a2e77857768439ced3c8f3
Artem Popov <aptem336@gmail.com>
2020-05-17 16:25:21 +0800

    cleanup

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,13 +1,10 @@
-$(document).ready(function()
-{
-
-$('.switch').click(function()
-{
-$(this).toggleClass("switchOn");
-if ($(this).hasClass('switchOn')) {
-                $('#canvas').css( "opacity", "1" );
-            } else {
-                $('#canvas').css( "opacity", "0" );
-            }
-});
+$(document).ready(function () {
+    $('.switch').click(function () {
+        $(this).toggleClass("switchOn");
+        if ($(this).hasClass('switchOn')) {
+            $('#canvas').css("opacity", "1");
+        } else {
+            $('#canvas').css("opacity", "0");
+        }
+    });
 });
--- scripts/setsize.js
+++ scripts/setsize.js
@@ -1,20 +1,20 @@
 var panel = document.getElementById("panel");
 var closer = document.getElementById("closer");
 
-var id1= document.getElementById('id1');
+var id1 = document.getElementById('id1');
 var elem1 = document.getElementById("elem1");
 var and = document.getElementById("and");
 var or = document.getElementById("or");
 var andnot = document.getElementById("andnot");
 var ornot = document.getElementById("ornot");
 var not = document.getElementById("not");
-var canvas=document.getElementById("canvas");
-var id2= document.getElementById('id2');
+var canvas = document.getElementById("canvas");
+var id2 = document.getElementById('id2');
 var elem2 = document.getElementById("elem2");
 var counter = document.getElementById("counter");
 var counter_ = document.getElementById("counter_");
 
-var id3= document.getElementById('id3');
+var id3 = document.getElementById('id3');
 var elem3 = document.getElementById("elem3");
 var analyzer1 = document.getElementById("analyzer1");
 var analyzer2 = document.getElementById("analyzer2");
@@ -26,68 +26,72 @@ var input_clk = document.getElementById("input_clk");
 
 var analyz = document.getElementById("analyz");
 var containeranalyzer = document.getElementById("containeranalyzer");
-var graf= document.getElementById('graf');
-var table= document.getElementById('table');
-var overlay=document.getElementById('overlay');
+var graf = document.getElementById('graf');
+var table = document.getElementById('table');
+var overlay = document.getElementById('overlay');
 
-panel.onclick = function(){
-	panel.classList.add("openpanel");
-	closer.classList.add("vision");
-	closer.classList.add("close2_show");
+panel.onclick = function () {
+    panel.classList.add("openpanel");
+    closer.classList.add("vision");
+    closer.classList.add("close2_show");
 };
 
-closer.onclick = function(){
-	panel.classList.remove("openpanel");
-	closer.classList.remove("close2_show");
+closer.onclick = function () {
+    panel.classList.remove("openpanel");
+    closer.classList.remove("close2_show");
 };
 
-id1.onclick = function(){
-	elem1.classList.toggle("elements_size");
-	id1.classList.toggle("elements_color");
-	and.classList.toggle("elements_color");
-	or.classList.toggle("elements_color");
-	andnot.classList.toggle("elements_color");
-	ornot.classList.toggle("elements_color");
-	not.classList.toggle("elements_color");
+id1.onclick = function () {
+    elem1.classList.toggle("elements_size");
+    id1.classList.toggle("elements_color");
+    and.classList.toggle("elements_color");
+    or.classList.toggle("elements_color");
+    andnot.classList.toggle("elements_color");
+    ornot.classList.toggle("elements_color");
+    not.classList.toggle("elements_color");
 };
-and.onclick = function(){	
+and.onclick = function () {
 
 };
 
-id2.onclick = function(){
-elem2.classList.toggle("elements_size");
-id2.classList.toggle("elements_color");
-counter.classList.toggle("elements_color");
-counter_.classList.toggle("elements_color");
+id2.onclick = function () {
+    elem2.classList.toggle("elements_size");
+    id2.classList.toggle("elements_color");
+    counter.classList.toggle("elements_color");
+    counter_.classList.toggle("elements_color");
 };
 
-id3.onclick = function(){
-	elem3.classList.toggle("elements_size");
-	id3.classList.toggle("elements_color");
-	analyzer1.classList.toggle("elements_color");
-	analyzer2.classList.toggle("elements_color");
+id3.onclick = function () {
+    elem3.classList.toggle("elements_size");
+    id3.classList.toggle("elements_color");
+    analyzer1.classList.toggle("elements_color");
+    analyzer2.classList.toggle("elements_color");
 };
 
-clk.onclick = function(){
-	clk_edit.classList.toggle("clkactive");
-	input_clk.classList.toggle("clk_hidden_input");
-	text_clk.classList.toggle("clk_hidden");
+clk.onclick = function () {
+    clk_edit.classList.toggle("clkactive");
+    input_clk.classList.toggle("clk_hidden_input");
+    text_clk.classList.toggle("clk_hidden");
 };
 
-analyz.onclick = function(){
-	analyz.classList.toggle("openanalyzer");
-	containeranalyzer.classList.toggle("openselectmenu");
+analyz.onclick = function () {
+    analyz.classList.toggle("openanalyzer");
+    containeranalyzer.classList.toggle("openselectmenu");
 };
-graf.onclick = function(){
-	graf.classList.add('selectanalyzerselected');
-	table.classList.remove('selectanalyzerselected');	
+graf.onclick = function () {
+    graf.classList.add('selectanalyzerselected');
+    table.classList.remove('selectanalyzerselected');
 }
-table.onclick = function(){
-	graf.classList.remove('selectanalyzerselected');
-	table.classList.add('selectanalyzerselected');	
+table.onclick = function () {
+    graf.classList.remove('selectanalyzerselected');
+    table.classList.add('selectanalyzerselected');
 };
 
-overlay.onclick = function(){
- $(function() {$( "#canvas" ).draggable();});
- $(function() {$( "#svgs" ).draggable({containment:'parent', grid: [15,15]});});
+overlay.onclick = function () {
+    $(function () {
+        $("#canvas").draggable();
+    });
+    $(function () {
+        $("#svgs").draggable({containment: 'parent', grid: [15, 15]});
+    });
 };  
