424adc7581168e11313bd73c94fed5856c140f95
prigorim <prigorim@gmail.com>
2020-05-21 15:04:09 +0800

    cleanup + scale 0.1

--- scripts/contextmenu.js
+++ scripts/contextmenu.js
@@ -5,19 +5,54 @@ $(document).ready(function () {
         if (event.which === 3) {
             var target = $(event.target);
             target.addClass('selected-html-element');
-            $('<div/>', {
-                class: 'context-menu'
-            })
-                .css({
-                    left: event.pageX + 'px',
-                    top: event.pageY + 'px'
+            if (target.is('#canvas')) {
+                $('<div/>', {
+                    class: 'context-menu'
                 })
-                .appendTo('body')
-                .append(
-                    $('<ul/>').append('<li><a href="#">Remove element</a></li>')
-                        .append('<li><a href="#">Add element</a></li>')
-                )
-                .show('fast');
+                    .css({
+                        left: event.pageX + 'px',
+                        top: event.pageY + 'px'
+                    })
+                    .appendTo('body')
+                    .append(
+                        $('<ul/>').append('<div id="add_element"><li><p>Добавить элемент</p></li></div>')
+                            .append('<div id="add_element"><li><p>Вернуть положение</p></li></div>')
+                            .append('<div id="add_element"><li><p>Восстановить масштаб</p></li></div>')
+                    )
+                    .show('fast');
+            }
+            if (target.is('#trash')) {
+                $('<div/>', {
+                    class: 'context-menu'
+                })
+                    .css({
+                        left: event.pageX + 'px',
+                        top: event.pageY + 'px'
+                    })
+                    .appendTo('body')
+                    .append(
+                        $('<ul/>').append('<div id="remove_element"><li><p>Открыть корзину</p></li></div>')
+                            .append('<div id="add_element"><li><p>Очистить корзину</p></li></div>')
+                    )
+                    .show('fast');
+            }
+            if (event.target.classList.contains('.autowidth')) {
+                $('<div/>', {
+                    class: 'context-menu'
+                })
+                    .css({
+                        left: event.pageX + 'px',
+                        top: event.pageY + 'px'
+                    })
+                    .appendTo('body')
+                    .append(
+                        $('<ul/>').append('<div id="remove_element"><li><p>Удалить элемент</p></li></div>')
+                            .append('<div id="add_element"><li><p>Изменить элемент</p></li></div>')
+                    )
+                    .show('fast');
+            }
+
         }
+
     });
 });
