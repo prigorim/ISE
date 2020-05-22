32a44ea80811ea09408f22b1ab2452aee1422019
prigorim <prigorim@gmail.com>
2020-05-22 21:21:03 +0800

    cleanup♂

--- scripts/load_save_new.js
+++ scripts/load_save_new.js
@@ -1,6 +1,7 @@
 const load = document.getElementById("load");
 const save = document.getElementById("save");
 const create = document.getElementById("create");
+const wrap = document.getElementById('wrap');
 
 load.onclick = function () {
     alert(2);
@@ -11,4 +12,9 @@ save.onclick = function () {
 
 create.onclick = function () {
     alert(3);
+    $('#canvas').remove();
+    let div = document.createElement('div');
+    div.className = "ui-draggable ui-draggable-handle checkered";
+    div.id="canvas";
+    wrap.append(div);//как сновы вызвать функцию? я не знаю
 }
