d2a8593a7e06073de60d610a43eca557484fb95f
prigorim <prigorim@gmail.com>
2020-05-22 20:31:41 +0800

    cleanup♂

---
+++ scripts/load_save_new.js
@@ -0,0 +1,14 @@
+const load = document.getElementById("load");
+const save = document.getElementById("save");
+const create = document.getElementById("create");
+
+load.onclick = function () {
+    alert(2);
+}
+save.onclick = function () {
+    alert(1);
+}
+
+create.onclick = function () {
+    alert(3);
+}
