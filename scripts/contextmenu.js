b2879e58a95802ed438aa11ec2e5aea436a4fcf8
prigorim <prigorim@gmail.com>
2020-06-01 15:03:20 +0800

    cleanup♂

--- scripts/contextmenu.js
+++ scripts/contextmenu.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         $('.context-menu').remove();
         if (event.which === 3) {
             var target = $(event.target);
-            //target.addClass('e');
+
             if (target.is('#canvas')) {
                 $('<div/>', {
                     class: 'context-menu'
@@ -15,14 +15,11 @@ $(document).ready(function () {
                     })
                     .appendTo('body')
                     .append(
-                        $('<ul/>').append('<div id="add_element"><li><p>Добавить элемент</p></li></div>')
-                            .append('<div id="return_move"><li><p>Вернуть положение</p></li></div>')
-                            .append('<div id="fix_scale"><li><p>Восстановить масштаб</p></li></div>')
+                        $('<ul/>').append('<li id="add_element"><p id="add_element">Добавить элемент</p>')
+                            .append('<li><p id="return_move">Вернуть положение</p></li></div>')
+                            .append('<li id="fix_scale"><p id="fix_scale">Восстановить масштаб</p></li>')
                     )
                     .show('fast');
-                $("#add_element").click(function () {
-                    alert(1);
-                });
             }
             if (target.is('#trash')) {
                 $('<div/>', {
@@ -64,10 +61,14 @@ $(document).ready(function () {
                     })
                     .appendTo('body')
                     .append(
-                        $('<ul/>').append('<div id="remove_element"><li><p>Удалить элемент</p></li></div>')
-                            .append('<div id="change_element"><li><p>Изменить элемент</p></li></div>')
+                        $('<ul/>').append('<li  id="remove_element"><p  id="remove_element">Удалить элемент</p></li>')
+                            .append('<li id="change_element"><p id="change_element">Изменить элемент</p></li>')
                     )
                     .show('fast');
+                $("#remove_element").click(function () {
+                    alert(1);
+                    target.remove();
+                });
             }
 
         }
