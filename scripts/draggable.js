cb88f05fa5d8402db699e930d8da44cf7f84c026
Artem Popov <aptem336@gmail.com>
2020-06-03 19:55:25 +0800

    correct draggable

--- scripts/draggable.js
+++ scripts/draggable.js
@@ -1,4 +1,3 @@
-var i = 0;
 document.onclick = function () {
     $(function () {
         const canvas = $("#canvas");
@@ -6,31 +5,24 @@ document.onclick = function () {
         $(".logic-element").draggable({
             appendTo: "#canvas",
             revert: 'invalid',
-            helper: "clone",
+            helper: () => new LogicElement(),
+            zIndex: 99
         });
-        $("#canvas").droppable({
-            accept: "#and, #or, #andnot, #ornot, #not, #xor, #counter, #counter_, #analyzer1, #analyzer2",
-            drop: function (ev, ui) {
-                let x = ui.position.left / 15;
-                let y = ui.position.top / 15;
-                let correctedX = Math.ceil(x) * 15;
-                let correctedY = Math.ceil(y) * 15;
-                $(ui.draggable).clone().prependTo(this)
-                    .removeClass()
-                    .addClass('added_element')
-                    .removeAttr('id')
-                    .draggable({
-                        grid: [15, 15],
-                        containment: 'parent',
-                    })
-                    .attr("id", 'log_' + i)
-                    .css({
-                        left: correctedX,
-                        top: correctedY,
-                        zIndex: 1,
-                        margin: 0,
-                    });
-                i = i + 1;
+        $(canvas).droppable({
+            accept: ".logic-element",
+            drop: function (e, ui) {
+                $(document.createElement(
+                    'table',
+                    {is: ui.draggable[0].getAttribute('is')})
+                ).css({
+                    position: 'absolute',
+                    left: ui.position.left - ui.position.left % 15,
+                    top: ui.position.top - ui.position.top % 15,
+                    zIndex: 10
+                }).draggable({
+                    grid: [15, 15],
+                    containment: 'parent',
+                }).prependTo(this);
             }
 
         });
