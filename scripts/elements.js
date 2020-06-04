47262d2b56daa6bf48b96750e41f81a606d77016
prigorim <prigorim@gmail.com>
2020-06-04 10:12:03 +0800

    styles for container

--- scripts/elements.js
+++ scripts/elements.js
@@ -132,11 +132,64 @@ class ElementLogicOr extends ElementLogic {
     }
 }
 
+class ElementLogicAndNot extends ElementLogic {
+    createSolid() {
+        let solid = super.createSolid();
+        solid.innerText = '&';
+        return solid;
+    }
+
+    func() {
+        //IMPL
+    }
+}
+
+class ElementLogicOrNot extends ElementLogic {
+    createSolid() {
+        let solid = super.createSolid();
+        solid.innerText = '1';
+        return solid;
+    }
+
+    func() {
+        //IMPL
+    }
+}
+
+class ElementLogicNot extends ElementLogic {
+    createSolid() {
+        let solid = super.createSolid();
+        solid.innerText = '1';
+        return solid;
+    }
+
+    func() {
+        //IMPL
+    }
+}
+
+class ElementLogicXor extends ElementLogic {
+    createSolid() {
+        let solid = super.createSolid();
+        solid.innerText = '=1';
+        return solid;
+    }
+
+    func() {
+        //IMPL
+    }
+}
+
 customElements.define('element-pin', Pin, {extends: 'div'});
 customElements.define('element-pin-input', PinPassive, {extends: 'div'});
 customElements.define('element-pin-output', PinFunctional, {extends: 'div'});
 
 customElements.define('element-ise', Element, {extends: 'table'});
 customElements.define('element-logic', ElementLogic, {extends: 'table'});
+
 customElements.define('element-logic-and', ElementLogicAnd, {extends: 'table'});
 customElements.define('element-logic-or', ElementLogicOr, {extends: 'table'});
+customElements.define('element-logic-andnot', ElementLogicAndNot, {extends: 'table'});
+customElements.define('element-logic-ornot', ElementLogicOrNot, {extends: 'table'});
+customElements.define('element-logic-not', ElementLogicNot, {extends: 'table'});
+customElements.define('element-logic-xor', ElementLogicXor, {extends: 'table'});
