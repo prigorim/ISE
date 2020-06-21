eae6fb177c01f218cc3831a573bc992b4d5ce46e
prigorim <prigorim@gmail.com>
2020-06-21 17:30:49 +0800

    save

--- scripts/load_save_new.js
+++ scripts/load_save_new.js
@@ -1,9 +1,20 @@
 const wrap = document.getElementById('wrap');
 
 $("#load").click(function () {
+$('#canvas').remove();
+    let div = document.createElement('div');
+    div.className = "ui-draggable ui-draggable-handle checkered";
+    div.id = "canvas";
+    wrap.append(div);
+    $("#canvas").draggable({});
 });
 
 $("#save").click(function () {
+a = document.createElement("a")
+a.setAttribute("href", "data:text/plain," + canvas.innerHTML)
+a.setAttribute("download", "scheme.txt")
+save.onclick = function(){ a.click() }
+
 });
 
 $("#create").click(function () {
